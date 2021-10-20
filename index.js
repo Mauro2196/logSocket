9d96a16b3f1ac0fccf11ad53e2d7bee602714d3a
Mauro <mauro2196@gmail.com>
2021-10-20 15:32:52 -0500

    my commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const net = require('net');
+
+let buffer = '';
+
+const server = net.createServer((c) => { // 'connection' listener
+console.log('server connected');
+c.on('end', () => {
+    console.log('server disconnected');
+});
+c.on('error', (error) => {
+    console.log('server error: ', error);
+});
+c.on('data', (data) => {
+    buffer += data.toString();
+    const itsEnd = buffer.slice(-5);
+    if (itsEnd === '[end]') {
+    const newStr = buffer.slice(7, -5);
+    console.log(newStr);
+    buffer = '';
+    }
+});
+});
+server.listen(8124, () => { // 'listening' listener
+console.log('server bound');
+});
+
