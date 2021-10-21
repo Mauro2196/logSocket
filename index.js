92560b0311ff1beff2e397d4367da232fee17878
Mauro <mauro2196@gmail.com>
2021-10-21 16:36:10 -0500

    adding server

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-const express = require("express");
+const express = require('express')
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || '3000';
-
-const app = express();
-let logs = ""
+let logs = []
+let logId = 0
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
-  }));
+}));
 
 app.get('/', (req, res) => {
-    res.send(logs)
+    res.sendFile(__dirname + '/index.html');
 });
 app.get('/logs', (req, res) => {
     res.send(logs)
 });
 app.post('/log', (req, res) => {
-    logs = `${logs} \n\r` + JSON.stringify(req.body)
+    logObj = { 
+        id: logId += 1, 
+        date: getdate(),
+        log: JSON.stringify(req.body)
+    };
+    logs.push(logObj);
     console.log(req.body)
+    io.emit('log message', logs);
     res.send("Ok")
 });
-app.listen(port, () => console.log(`server started on port ${port}`));
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+function getdate() {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    return dateTime
+}
